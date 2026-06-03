decc87f5aa8254e1f6b5c30c675bc64e8a8f9902
Sazista <leo@starshipcanada.com>
2026-06-03 14:05:44 -0400

    Add: assets/wp-content/uploads/js-cache-.js

---
+++ assets/wp-content/uploads/js-cache-.js
@@ -0,0 +1,13 @@
+    (function($){
+    $(document).ready(function() {
+
+        
+        $('.parallax').each(function(){
+           $(this).parallax("50%", 0.4);
+        });
+
+        
+        !function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/5198aa44828f5f0e47803b8f9/778db5f8428a334ef19a03cc5.js");
+    });
+    })(jQuery);
+     /*2019-05-01 01:54*//* JS Generator Execution Time: 8.2969665527344E-5 seconds */
